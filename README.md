f6c7d99eb5b3c295e55f2be7c3df162210bea774
Peter Law <PeterJCLaw@gmail.com>
2014-03-14 19:18:13 +0000

    Initial attempt at a dev wrapper.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 sr-comp-dev
 ===========
 
-Development wrapper to the SRComp suite of Competition software
+Development wrapper to the SRComp suite of Competition software.
+
+This is a wrapper which makes it easier to develop things.
+No real code should be in here.
---
+++ app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, PATH + "/http/")
+
+from app import app
+
+@app.route("/screen")
+def screen():
+    return open(PATH + '/screens/screen.html').read()
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description = "SR Competition info Development API HTTP server")
+    parser.add_argument("-c", "--compstate", default=PATH + '/compstate',
+                        help = "Competition state git repository path")
+    parser.add_argument("-p", "--port", type=int, default=5112,
+                        help = "Port to listen on")
+    args = parser.parse_args()
+
+    app.config["COMPSTATE"] = args.compstate
+    app.debug = True
+    app.run(host='0.0.0.0', port=args.port)

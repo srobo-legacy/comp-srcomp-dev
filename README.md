9669f690fa02543a54f6da8bb73a781684052302
Alistair Lynn <arplynn@gmail.com>
2015-01-30 12:36:51 +0000

    Convert to a script matching the setuptools system
    Rather than having a root of a submodule tree, this repo now has a
    script which fetches and configures all the relevant parts of the srcomp
    infrastructure.
    
    Patch Set #2: Add README, run-all-tests, and clone some submodules
    
    Change-Id: I0c44d46c6586fd74df51eeda42fc482519c85ac6

--- README.md
+++ README.md
@@ -1,31 +1,20 @@
-srcomp-dev
-===========
+SRcomp Development Script
+=========================
 
-Development wrapper to the SRComp suite of Competition software.
+This repository contains a script—`init.sh`—which builds an srcomp
+development environment.
 
-This is a wrapper which makes it easier to develop things.
-No real code should be in here.
+Specifically, when, run it:
 
-See https://www.studentrobotics.org/trac/wiki/Competition/Software for a
-rough guide on the overall aims.
+ * creates a virtualenv and configures it with external dependencies,
+ * clones and configures the following repositories:
+   * `ranker`, the game-points-to-league-points system;
+   * `srcomp`, the core srcomp library;
+   * `srcomp-http`, the srcomp REST API;
+   * `srcomp-scorer`, the srcomp score entry system,
+   * `srcomp-screens`, the repository containing the arena display HTML,
+   * `srcomp-stream`, the system that streams events live,
+   * `dummy-comp`, the standard testing compstate repository
 
-**Warning:** This repo has several (at least four) layers of submodules.
-Be sure you've got all them all checked out -- you'll get import errors
-from most things, but there are also some symlinks which cross module
-boundaries which will likely give other errors.
+It then emits instructions on how to use the virtualenv.
 
-## Run with
-`./app.py`
-
-Serves the competition API plus some related pages (so they don't need
-to worry about having XSS issues) at http://localhost:5112.
-
-If you want to develop the srweb competition mode stuff, you'll need to
-arrange for http://localhost/comp-api to be routed to the above, which
-whatever you're using to serve srweb will probably be able to do.
-
-## Test with
-`./run-all-tests`
-
-Recursively looks for files called `run-tests`, and then tries to
-run them in their own directories. Bails after the first failure.
---
+++ init.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+if [ -f ~/.ssh/config ]; then
+    GERRIT=$(grep -m 1 -B 5 29418 ~/.ssh/config | grep -E '^Host ' | tail -1 | sed 's/Host //')
+    GITHUB=$(grep -m 1 -B 5 'github.com' ~/.ssh/config | grep -E '^Host ' | tail -1 | sed 's/Host //')
+fi
+
+if [ -n "$GERRIT" ]; then
+    echo "Detected GERRIT as $GERRIT"
+    function clone_srobo {
+        git clone --recursive $GERRIT:$1 $2
+    }
+else
+    function clone_srobo {
+        git clone --recursive ssh://studentrobotics.org:29418/$1 $2
+    }
+fi
+if [ -n "$GITHUB" ]; then
+    echo "Detected GITHUB as $GITHUB"
+    function clone_gh {
+        git clone --recursive $GITHUB:$1 $2
+    }
+else
+    function clone_gh {
+        git clone --recursive https://github.com/$1 $2
+    }
+fi
+
+for POSSIBLE_PYTHON in python3 python;
+do
+    PYTHON=$(which $POSSIBLE_PYTHON)
+    $POSSIBLE_PYTHONS --version 2>&1 | grep 'Python 3\.' >/dev/null
+    if [ $? -ne 0 ]; then
+        echo "Found Python: $PYTHON"
+        break
+    fi
+done
+if [ -z "$PYTHON" ]; then
+    echo "No Python installation found."
+    exit 1
+fi
+
+set -e
+$PYTHON -m venv venv
+source venv/bin/activate
+set -v
+pip install -r requirements.txt
+clone_srobo comp/ranker.git ranker
+clone_srobo comp/srcomp.git srcomp
+clone_srobo comp/srcomp-http.git srcomp-http
+clone_srobo comp/srcomp-screens.git srcomp-screens
+clone_srobo comp/dummy-comp.git dummy-comp
+clone_srobo comp/srcomp-scorer.git srcomp-scorer
+clone_gh prophile/srcomp-stream.git srcomp-stream
+cd ranker
+    python setup.py develop
+cd ..
+cd srcomp
+    python setup.py develop
+cd ..
+cd srcomp-http
+    python setup.py develop
+cd ..
+cd srcomp-scorer
+    python setup.py develop
+cd ..
+cd srcomp-screens
+    git checkout rewrite
+    bower install
+cd ..
+cd srcomp-stream
+    sed 's_SRCOMP: .*_SRCOMP: "http://localhost:5112"_' <config.coffee.example >config.coffee
+    npm install
+cd ..
+set +v
+echo "-- DONE SETUP --"
+echo "Usage: "
+echo "  (1) Activate the virtualenv: source venv/bin/activate"
+echo "  (2) Run things"
+

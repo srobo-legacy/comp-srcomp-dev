bf8f67b9d87a9c7911665378e3eea7272f9b483c
Tom Leese <inbox@tomleese.me.uk>
2015-04-16 15:08:56 +0100

    Add a reset script

---
+++ reset.sh
@@ -0,0 +1,17 @@
+echo 'Warning: This will reset all repos and checkout master.'
+echo 'Press <return> to confirm.'
+read
+
+for repo in dummy-comp sr2014-comp sr2015-comp ranker srcomp srcomp-http srcomp-scorer srcomp-screens srcomp-stream srcomp-cli; do
+    echo $repo
+    echo ------------------
+    cd $repo
+        git reset --hard HEAD
+	git checkout master
+    cd ..
+    echo
+done
+
+echo srcomp-dev
+echo ------------------
+git pull --ff-only

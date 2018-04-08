5312e321e49d27be7f86a7d631fb16e895ee5eca
Peter Law <PeterJCLaw@gmail.com>
2018-04-08 18:54:16 +0100

    Cope with missing subfolders
    Change-Id: I380bdb5f1b7fe3de436f04ef3cedd552f417d15f

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 for repo in dummy-comp sr2014-comp sr2015-comp sr2016-comp ranker srcomp srcomp-http srcomp-scorer srcomp-screens srcomp-stream srcomp-cli srcomp-kiosk; do
     echo $repo
     echo ------------------
-    cd $repo
+    pushd $repo && \
         git pull --ff-only
-    cd ..
+    popd
     echo
 done
 
